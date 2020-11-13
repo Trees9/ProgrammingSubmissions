4f1dcfe522ebc981e1534023faa369c631bc0b82
aryanindarapu <aryan1pro@gmail.com>
2020-11-12 20:23:26 -0500

    added newest code files

---
+++ TestCode.java
@@ -0,0 +1,56 @@
+public class TestCode {
+    public static char nameWhatever () {
+        return 5.5;
+    }
+
+    public static void main(String []args) {
+        int x = 7;
+        int a = 2;
+
+        System.out.print("The value of a + x is: " + a + x);
+
+        // if (x == 0) {
+        //     System.out.println("x is 0");
+        // } else {
+        //     System.out.println("x is not 0");
+        // }
+
+
+        
+        // Implemented as a if-else ladder
+        // if (a == -2) {
+        //     System.out.println("a is -2!");
+        // } else if (a == -1) {
+        //     System.out.println("a is -1!");
+        // } else if (a == 0) {
+        //     System.out.println("a is 0!");
+        // } else if (a == 1) {
+        //     System.out.println("a is 1!");
+        // } else if (a == 2) {
+        //     System.out.println("a is 2!");
+        // }
+
+        // Implemented as a switch statement
+        // switch (a) {
+        //     case -2: 
+        //         System.out.println("a is -2!");
+        //         break;
+        //     case -1: 
+        //         System.out.println("a is -1!");
+        //         break;
+        //     case 0: 
+        //         System.out.println("a is 0!");
+        //         break;
+        //     case 1: 
+        //         System.out.println("a is 1!");
+        //         break;
+        //     case 2: 
+        //         System.out.println("a is 2!");
+        //         break;
+        // }
+
+        // for (int i = 0; i < 5; i += 2) {
+        //     System.out.println(i);
+        // }
+    }
+}
